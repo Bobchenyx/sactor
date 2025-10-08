6bb1016161fa6205e2d7d1211a7c264930b5b0b6
Bobchenyx <chen.yixia@northeastern.edu>
2025-10-08 12:04:11 -0400

    add setup-path.sh

---
+++ setup-path.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export PATH="/home/yixiao/cToRust/c2rust/target/release:$PATH"
+export PATH="/home/yixiao/cToRust/crown/target/release:$PATH"
+
+# git clone -b sactor https://github.com/qsdrqs/crown.git
+# cd crown
+# cargo build --release
+# cd ..
+
+# git clone https://github.com/immunant/c2rust.git
+# cd c2rust
+# cargo build --release
+# cd ..
+
+# export PATH="<PATH>/c2rust/target/release:$PATH"
+# export PATH="<PATH>/crown/target/release:$PATH"
